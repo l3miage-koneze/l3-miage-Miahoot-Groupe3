a72e677201c65b158ea10ef8d076ed3d00d5cad4
l3miage-diabyfa <diabyfatoumat6@gmail.com>
2023-04-26 08:23:51 +0200

    Ajout endpoint et controller Questions

--- rest-api/src/main/java/fr/uga/l3miage/example/endpoint/QuestionEndpoint.java
+++ rest-api/src/main/java/fr/uga/l3miage/example/endpoint/QuestionEndpoint.java
@@ -4,6 +4,8 @@ import fr.uga.l3miage.example.annotations.Error400Custom;
 import fr.uga.l3miage.example.error.TestEntityNotDeletedErrorResponse;
 import fr.uga.l3miage.example.error.TestNotFoundErrorResponse;
 import fr.uga.l3miage.example.request.CreateTestRequest;
+import fr.uga.l3miage.example.response.QuestionDto;
+import fr.uga.l3miage.example.response.ReponseDto;
 import fr.uga.l3miage.example.response.Test;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -17,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
 
 @Tag(name = "Question tag")
 @CrossOrigin
@@ -24,51 +28,66 @@ import javax.validation.Valid;
 @RequestMapping("question/")
 public interface QuestionEndpoint {
 
-    @Operation(description = "Récupérer la phrase 'Hello word'")
-    @ApiResponse(responseCode = "200", description = "Si isInError est à false alors 'Hello word' est renvoyé.",
-            content = @Content(schema = @Schema(implementation = String.class), mediaType = MediaType.TEXT_PLAIN_VALUE))
-    @ApiResponse(responseCode = "500", description = "Si isInError est à true alors nous avons une erreur 500 qui est renvoyée.",
+    /*
+    @Operation(description = "Récupérer les questions ")
+    @ApiResponse(responseCode = "200", description = "recuperation des questions reussis.",
+            content = @Content(schema = @Schema(implementation = QuestionDto.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @ApiResponse(responseCode = "500", description = "recuperation non reussis renvoie erreur",
             content = @Content(schema = @Schema(implementation = String.class), mediaType = MediaType.TEXT_PLAIN_VALUE))
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("hello")
-    ResponseEntity<String> getHelloWord(@Parameter(description = "correspond a si vous voulez avoir une erreur ou non" ,example = "true") @RequestParam boolean isInError);
-
-
-    @Operation(description = "Récupérer le DTO de l'entité test qui a pour id celui passé en paramètre")
-    @ApiResponse(responseCode = "200", description = "Renvoie le DTO de l'entité test demandée",
+    @GetMapping("question")
+    Collection<QuestionDto> getAllQuestions()(@RequestParam QuestionDto questionDto);
+*/ // N'est pas gerer par les service
+//ok
+    @Operation(description = "Récupérer le DTO de l'entité question qui a pour id celui passé en paramètre")
+    @ApiResponse(responseCode = "200", description = "Renvoie le DTO de l'entité question demandée",
             content = @Content(schema = @Schema(implementation = Test.class),mediaType = MediaType.APPLICATION_JSON_VALUE))
     @ApiResponse(responseCode = "404", description = "Renvoie une erreur 404 si l'entité n'est pas trouvée",
             content = @Content(schema = @Schema(implementation = TestNotFoundErrorResponse.class),mediaType = MediaType.APPLICATION_JSON_VALUE))
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("{description}")
-    Test getEntityTest(@PathVariable String description);
+    @GetMapping("/question/{id}")
+    QuestionDto getQuestion(@PathVariable Long id);
 
-
-    @Operation(description = "Création d'une entité Test")
-    @ApiResponse(responseCode = "201", description = "L'entité Test a bien été créée.")
+//Ok
+    @Operation(description = "Création d'une entité Question")
+    @ApiResponse(responseCode = "201", description = "L'entité question a bien été créée.")
     @Error400Custom
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    void createEntityTest(@Valid @RequestBody CreateTestRequest request);
+    @PostMapping(value = "/question")
+    void newQuestion(@Valid @RequestBody QuestionDto questionDto);
 
 
 
-    @Operation(description = "Mise à jour d'une entité test")
+    @Operation(description = "Mise à jour d'une entité question")
     @ApiResponse(responseCode = "202", description = "L'entité à bien été mis à jour")
     @ApiResponse(responseCode = "404", description = "Renvoie une erreur 404 si l'entité n'est pas trouvée",
             content = @Content(schema = @Schema(implementation = TestNotFoundErrorResponse.class),mediaType = MediaType.APPLICATION_JSON_VALUE))
     @Error400Custom
     @ResponseStatus(HttpStatus.ACCEPTED)
-    @PatchMapping("{lastDescription}")
-    void updateTestEntity(@PathVariable final String lastDescription,@RequestBody final Test test);
+    @PatchMapping("{Questions/{id}")
+    void updateQuestion(@PathVariable Long id,@RequestBody @Valid QuestionDto questionDto);
 
 
 
-    @Operation(description = "Suppression d'une entité Test en bd")
-    @ApiResponse(responseCode = "200", description = "si isInError est à false alors 'Hello word' est renvoyé")
+    @Operation(description = "Suppression d'une entité question en bd")
+    @ApiResponse(responseCode = "200", description = "si isInError est à false alors la question est renvoyé")
     @ApiResponse(responseCode = "418", description = "Renvoie une erreur 418 si l'entité n'a pu être supprimée",
             content = @Content(schema = @Schema(implementation = TestEntityNotDeletedErrorResponse.class),mediaType = MediaType.APPLICATION_JSON_VALUE))
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping("{description}")
-    void deleteTestEntity(@PathVariable String description);
+    @DeleteMapping("/Questions/{id}")
+    void deleteQuestion(@PathVariable Long id);
+
+/*
+    @Operation(description = "Récupérer les reponses possibles d'une question ")
+    @ApiResponse(responseCode = "200", description = "recuperation des reponses reussis.",
+            content = @Content(schema = @Schema(implementation = QuestionDto.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @ApiResponse(responseCode = "500", description = "recuperation non reussis renvoie erreur",
+            content = @Content(schema = @Schema(implementation = String.class), mediaType = MediaType.TEXT_PLAIN_VALUE))
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/Questions/{id}/Reponses")
+    Collection<ReponseDto> getAllReponses(@PathVariable("id") @NotNull Long questionId);
+*/ // N'est pas gerer par les service
+
 }
+
+
--- server/src/main/java/fr/uga/l3miage/example/controller/QuestionController.java
+++ server/src/main/java/fr/uga/l3miage/example/controller/QuestionController.java
@@ -1,119 +1,50 @@
 package fr.uga.l3miage.example.controller;
 
 import fr.uga.l3miage.example.endpoint.QuestionEndpoint;
-import fr.uga.l3miage.example.exception.rest.IsInErrorRestException;
 import fr.uga.l3miage.example.mapper.QuestionMapper;
-import fr.uga.l3miage.example.mapper.ReponseMapper;
 import fr.uga.l3miage.example.models.QuestionEntity;
-import fr.uga.l3miage.example.request.CreateTestRequest;
-import fr.uga.l3miage.example.response.MiahootDto;
 import fr.uga.l3miage.example.response.QuestionDto;
 import fr.uga.l3miage.example.response.ReponseDto;
-import fr.uga.l3miage.example.response.Test;
 import fr.uga.l3miage.example.service.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
-import java.util.stream.Collectors;
 import java.util.Collection;
-
 @RestController
 @RequiredArgsConstructor
 public class QuestionController implements QuestionEndpoint {
 
     private final QuestionService questionService;
     private final QuestionMapper questionMapper;
-    private final ReponseMapper reponseMapper;
+    /*
 
-    @PostMapping(value = "/Questions")
-    @ResponseStatus(HttpStatus.CREATED)
-    public QuestionDto newQuestion(@RequestBody @Valid QuestionDto questionDto){
-        //QuestionEntity QuestionEntity = QuestionMapper.toQuestionEntity(QuestionDto);
-        if(questionDto == null){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST , null);
-        }
-        try{
-            QuestionEntity questionEntity = this.questionService.save(questionMapper.toQuestionEntity(questionDto));
-            return questionMapper.toQuestionDto(questionEntity);
-        } catch(Exception e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null , e);
-        }
+    public Collection<QuestionDto> getAllQuestions() {
+        return null;
+    } */
+
+    @Override
+    public QuestionDto getQuestion(Long id) {
+        return questionService.getQuestion(id);
     }
 
-    @GetMapping("/Questions/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public QuestionDto getQuestion(@PathVariable("id") Long id){
-        try{
-            QuestionEntity questionEntity= this.questionService.get(id);
-            return this.questionMapper.toQuestionDto(questionEntity);
-        } catch(Exception e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null , e);
-        }
+    @Override
+    public void newQuestion(QuestionDto questionDto) {
+        QuestionEntity ques = questionMapper.toQuestionEntity(questionDto);
+         questionService.createQuestion(ques);
+    }
+
+    @Override
+    public void updateQuestion(Long id, QuestionDto questionDto) {
+        questionService.updateQuestion(id, questionDto);
+    }
+
+    @Override
+    public void deleteQuestion(Long id) {
+        questionService.deleteQuestion(id);
     }
 
 
-@GetMapping("/Questions")
-public Collection<QuestionDto> getAllQuestions() {
-   return questionMapper.toQuestionDto(questionService.list());
+    /*public Collection<ReponseDto> getAllReponses(Long questionId) {
+        return null;
+    }*/
 }
-
-@GetMapping("/Questions")
-public Collection<QuestionDto> getQuestionsByLabel(@RequestParam(value = "q", required = false) String query){
-    Collection<QuestionEntity> questionEntities;
-    if (query == null) {
-        questionEntities = questionService.list();
-    } else {
-        questionEntities = questionService.searchByLabel(query);
-    }
-    return questionEntities.stream()
-            .map(questionMapper::toQuestionDto)
-            .toList();
-}
-
-@DeleteMapping("/Questions/{id}")
-@ResponseStatus(HttpStatus.NO_CONTENT)
-public void deleteQuestion(@PathVariable("id") Long id) throws Exception{
-    try{
-        questionService.delete(id);
-    }catch(Exception e){
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The Question was not found");
-    }
-}
-
-
-
-    @PutMapping("Questions/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public QuestionDto updateQuestion(@PathVariable("id") @NotNull Long id, @RequestBody @Valid QuestionDto questionDto){
-        try {
-            if (questionDto.id().equals(id)) {
-                QuestionEntity questionEntity = (QuestionEntity) questionMapper.toQuestionEntity(questionDto);
-                var updated = questionService.update(questionEntity);
-                return questionMapper.toQuestionDto(updated);
-            }
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
-        } catch (IllegalArgumentException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, null, e);
-        }
-    }
-
-    @GetMapping("/Questions/{id}/Reponses")
-    public Collection<ReponseDto> getAllReponses(@PathVariable("id") @NotNull Long questionId) {
-        try {
-            var question = questionService.get(questionId);
-            return reponseMapper.toReponseDto(question.getReponses());
-        } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
-        }
-    }
-    }
